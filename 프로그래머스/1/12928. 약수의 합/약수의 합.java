5000129be47086c5b249829db5a86e0d79f7a3aa
teawoong Kim <116135174+ultramancode@users.noreply.github.com>
2023-11-23 19:56:39 +0900

    [level 1] Title: 약수의 합, Time: 0.11 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 1; i<=n; i++){
+            if(n%i==0){
+                answer+=i;
+            }
+        }
+        return answer;
+    }
+}
