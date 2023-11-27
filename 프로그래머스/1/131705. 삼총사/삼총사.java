93c90419d0b825b4680f9685ff8e4c4729ecf6e0
teawoong Kim <116135174+ultramancode@users.noreply.github.com>
2023-11-27 14:32:21 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        int n = number.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
