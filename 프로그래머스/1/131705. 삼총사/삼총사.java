516385036d5e3306b95f84215e7af1fa98b16e9b
teawoong Kim <116135174+ultramancode@users.noreply.github.com>
2023-11-27 15:29:58 +0900

    [level 1] Title: 삼총사, Time: 0.08 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,19 +1,37 @@
-import java.util.*;
-
 class Solution {
+
+    static int count;
+    static boolean[] visited;
+
     public int solution(int[] number) {
         int answer = 0;
-        int n = number.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (number[i] + number[j] + number[k] == 0) {
-                        answer++;
-                    }
+        visited = new boolean[number.length];
+
+        dfs(number, number.length, 0, 3); // nCr 3개 고르기
+        answer = count;
+        return answer;
+    }
+
+    public static void dfs(int[] numbers, int n, int start, int r){
+        if(r == 0){
+            int sum =0;
+            for(int i =0 ; i< n; i++){
+                if(visited[i]){
+                    sum += numbers[i];
                 }
             }
+            if(sum == 0){
+                count++;
+            }
+            return;
         }
 
-        return answer;
+        for(int i = start ; i< n; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                dfs(numbers, n, i, r-1 );
+                visited[i] = false;
+            }
+        }
     }
 }
