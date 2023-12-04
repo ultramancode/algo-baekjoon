354862f26b44bb1d9079b729d9e20108f9022532
teawoong Kim <116135174+ultramancode@users.noreply.github.com>
2023-12-05 08:18:13 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.46 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,20 +1,19 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i =0; i<score.length; i++){
-            arr.add(score[i]);
-            Collections.sort(arr, Collections.reverseOrder());
 
-            if(i<k){
-               answer[i] = arr.get(arr.size()-1);
-            }else{
-                answer[i] = arr.get(k-1);
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        for (int i = 0; i < score.length; i++) {
+            priorityQueue.add(score[i]);
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
             }
+            answer[i] = priorityQueue.peek();
         }
-        
+
         return answer;
     }
 }
