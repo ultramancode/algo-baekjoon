eefc5d6aefc0fac452bed5f451969994029af6f6
teawoong Kim <116135174+ultramancode@users.noreply.github.com>
2023-12-07 09:02:06 +0900

    [level 1] Title: 과일 장수, Time: 0.44 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -2,9 +2,9 @@ import java.util.*;
  
 class Solution {
     public int solution(int k, int m, int[] score) {
-        int answer = 0;
+        int answer = 0;        
         Arrays.sort(score);
- 
+        // 이 문제에선 score.length-1로 하면 기준점이 바뀌니..
         for(int i = score.length; i >= m; i -= m){
             answer += m * score[i - m];
         }
