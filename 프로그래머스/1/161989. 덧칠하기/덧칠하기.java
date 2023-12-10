3d179a3702b1487abe6e785f045af501b2a8b21d
teawoong Kim <116135174+ultramancode@users.noreply.github.com>
2023-12-10 18:44:50 +0900

    [level 1] Title: 덧칠하기, Time: 1.32 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -5,9 +5,9 @@ class Solution {
         int answer =0;
 
         for(int i=0; i<section.length; i++){
-            if(tmp <= section[i]){
+            if(tmp < section[i]){
                 answer++;
-                tmp = section[i]+m;
+                tmp = section[i]+m-1;
             } else continue;
         }
          return answer;
