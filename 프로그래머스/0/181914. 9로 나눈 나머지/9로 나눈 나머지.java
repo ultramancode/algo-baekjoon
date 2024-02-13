004f00ece00bd4decd3f2be97a99cd07af404171
teawoong Kim <116135174+ultramancode@users.noreply.github.com>
2024-02-13 10:47:04 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 1.70 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int solution(String number) {
+
+       return number.chars()
+            .map(Character::getNumericValue)
+            .sum() % 9;
+
+
+
+    }
+}
