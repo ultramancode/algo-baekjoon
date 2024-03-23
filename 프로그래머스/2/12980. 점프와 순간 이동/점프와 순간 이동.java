dfaeb7e16f859eeb7801fed9b1e995ee246febe9
teawoong Kim <116135174+ultramancode@users.noreply.github.com>
2024-03-23 13:43:46 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+     int answer = 0;
+        while(n != 0){
+            if(n%2==0){
+                n=n/2;
+            }else{
+                n-=1;
+                answer += 1;
+            }
+        }
+        return answer;
+
+    }
+}
