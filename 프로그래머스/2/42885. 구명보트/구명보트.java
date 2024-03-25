299ee972e2b26e8a5216aee5f3338c532d308651
teawoong Kim <116135174+ultramancode@users.noreply.github.com>
2024-03-25 15:57:17 +0900

    [level 2] Title: 구명보트, Time: 8.91 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+      public int solution(int[] people, int limit) {
+          
+          Arrays.sort(people);
+          int answer = 0;
+          int left = 0;
+          int right = people.length-1;
+          
+          while(left<=right){
+              if(people[left] + people[right] <= limit){
+                  left++;
+              }
+              right --;
+              // 몸무게 많이 나가면 혼자 보트
+              answer++;
+          }
+          return answer;
+      }
+}
+          
+          
+          
+          
+          
+          
+          
+   
