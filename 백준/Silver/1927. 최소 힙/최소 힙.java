fca881ed4d161f7d210edc0ee0aff3a2cfd000df
teawoong Kim <116135174+ultramancode@users.noreply.github.com>
2025-05-02 01:02:38 +0900

    [Silver II] Title: 최소 힙, Time: 1368 ms, Memory: 109780 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
+                }
+            } else {
+                pq.offer(x);
+            }
+        }
+    }
+}
