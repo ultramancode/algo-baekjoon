4e91438e69d30439fb5b32ea12e81b18e365b1f1
teawoong Kim <116135174+ultramancode@users.noreply.github.com>
2025-05-28 01:21:16 +0900

    [Gold IV] Title: 플로이드, Time: 380 ms, Memory: 41976 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int m;
+
+    static int[][] dist;
+
+    // 문제조건 99*100,000보다 큰 수여야..
+    static int INF = 100000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        dist = new int[n+1][n+1];
+        // 1번 도시부터
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            dist[a][b] = Math.min(dist[a][b], c);
+        }
+
+        for (int k = 1; k <=n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if(dist[i][j] > dist[i][k] + dist[k][j]){
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(dist[i][j] == INF){
+                    sb.append("0 ");
+                } else {
+                    sb.append(dist[i][j]).append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
